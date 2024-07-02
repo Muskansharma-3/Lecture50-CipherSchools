91089fa42f4c3dc243efb3899ba32c850a7dd2f8
Muskansharma-3 <119069933+Muskansharma-3@users.noreply.github.com>
2024-07-02 12:36:03 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LoginScreen from "./screens/LoginScreen";
+
+const router = createBrowserRouter([
+    {
+        path: "/login",
+        element: <LoginScreen />
+    }
+])
+
+const App = () => {
+    return <RouterProvider router={router} />
+}
+
+export default App;
